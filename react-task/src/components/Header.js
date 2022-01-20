556642c05a76534817bb8586bcd5fff98f86efe1
Oscar <oscar.andres.vargas1@gmail.com>
2022-01-20 11:48:08 +0100

    task reminder

--- react-task/src/components/Header.js
+++ react-task/src/components/Header.js
@@ -1,9 +1,15 @@
 import PropTypes from 'prop-types';
+import Button from './Button';
 
 const Header = ({ title }) => {
+  // { title } -> destructure of props
+  const onClick = () => {
+    console.log('Click');
+  };
   return (
-    <header>
+    <header className="header">
       <h1>{title}</h1>
+      <Button color="green" text="Add" onClick={onClick} />
     </header>
   );
 };
